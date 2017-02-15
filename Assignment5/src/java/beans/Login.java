599df548c1e7a516633142514017db60b0bd8d9d
hunnysaini <saininpr24@gmail.com>
2017-02-15 17:07:17 -0500

    Login bean completed

--- Assignment5/src/java/beans/Login.java
+++ Assignment5/src/java/beans/Login.java
@@ -11,17 +11,62 @@ import javax.enterprise.context.SessionScoped;
 
 /**
  *
- * @author c0687799
+ * @author c0665354
  */
-@Named(value = "login")
+@Named
 @SessionScoped
-
 public class Login implements Serializable {
+private String username;
+private String password;
+private boolean loggedIn;
+private User currentUser;
 
-    /**
-     * Creates a new instance of Login
-     */
     public Login() {
+        username = null;
+        password = null;
+        loggedIn = false;
+        currentUser = null;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
+
+
+    public User getCurrentUser() {
+        return currentUser;
+    }
+    
+    public String login() {
+    String passhash = DBUtils.hash(password);
+    
+    for (User u : Users.getInstance().getUsers()) {
+        if(username.equals(u.getUsername()) 
+         && passhash.equals(u.getPasshash())) {
+        loggedIn = true;
+        currentUser = u;
+        return "index";
+        }
+    }
+    currentUser = null;
+        loggedIn = false;
+        return "index";
     }
     
 }
