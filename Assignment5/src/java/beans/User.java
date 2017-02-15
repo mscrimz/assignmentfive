bc52be820d5fc9cf55fe044b8b260ea71a44f33c
wils1717 <mark_wilson17@live.ca>
2017-02-15 16:47:44 -0500

    added users beans

--- Assignment5/src/java/beans/User.java
+++ Assignment5/src/java/beans/User.java
@@ -5,121 +5,40 @@
  */
 package beans;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.sql.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Named;
-
-/**
- *
- * @author Len Payne <len.payne@lambtoncollege.ca>
- */
-@Named
-@ApplicationScoped
 public class User {
 
-    private List<User> user;
-    private static User instance;
+    private int id;
+    private String username;
+    private String passhash;
 
-    /**
-     * No-arg constructor -- retrieves List from DB and sets up singleton
-     */
-    public User() {
-        getUsersFromDB();
-        instance = this;
+    public User(int id, String username, String passhash) {
+        this.id = id;
+        this.username = username;
+        this.passhash = passhash;
     }
 
-    /**
-     * Retrieve the List of Users from the DB
-     */
-    private void getUsersFromDB() {
-        try (Connection conn = DBUtils.getConnection()) {
-            user = new ArrayList<>();
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM users");
-            while (rs.next()) {
-                User u = new User(
-                        rs.getInt("id"),
-                        rs.getString("username"),
-                        rs.getString("passhash")
-                );
-                user.add(u);
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
-            // This Fails Silently -- Sets User List as Empty
-            user = new ArrayList<>();
-        }
+    public int getId() {
+        return id;
     }
 
-    /**
-     * Retrieve the List of User objects
-     *
-     * @return the List of User objects
-     */
-    public List<User> getUser() {
-        return user;
+    public void setId(int id) {
+        this.id = id;
     }
 
-    /**
-     * Retrieve the known instance of this class
-     *
-     * @return the known instance of this class
-     */
-    public static User getInstance() {
-        return instance;
+    public String getUsername() {
+        return username;
     }
 
-    /**
-     * Retrieve a specific username by ID
-     *
-     * @param id the ID to search for
-     * @return the username
-     */
-    public String getUsernameById(int id) {
-        for (User u : user) {
-            if (u.getId() == id) {
-                return u.getUsername();
-            }
-        }
-        return null;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    /**
-     * Retrieve a specific user ID by username
-     *
-     * @param username the username to search for
-     * @return the user ID
-     */
-    public int getUserIdByUsername(String username) {
-        for (User u : user) {
-            if (u.getUsername().equals(username)) {
-                return u.getId();
-            }
-        }
-        return -1;
+    public String getPasshash() {
+        return passhash;
     }
 
-    /**
-     * Add a user to the DB
-     *
-     * @param username the username
-     * @param password the password
-     */
-    public void addUser(String username, String password) {
-        try (Connection conn = DBUtils.getConnection()) {
-            String passhash = DBUtils.hash(password);
-            String sql = "INSERT INTO users (username, passhash) VALUES(?,?)";
-            PreparedStatement pstmt = conn.prepareStatement(sql);
-            pstmt.setString(1, username);
-            pstmt.setString(2, passhash);
-            pstmt.executeUpdate();
-        } catch (SQLException ex) {
-            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        getUsersFromDB();
+    public void setPasshash(String passhash) {
+        this.passhash = passhash;
     }
+
 }
